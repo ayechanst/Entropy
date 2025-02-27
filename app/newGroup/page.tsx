a72db0841ae4cfddce56b54dcc330afd0a837831
ayechan <ayechan.sant@gmail.com>
2025-02-26 16:41:35 -0800

    form submits to supabase properly

--- app/newGroup/page.tsx
+++ app/newGroup/page.tsx
@@ -1,13 +1,63 @@
 "use client";
 import { useState } from "react";
 import { useSession } from "next-auth/react";
+import { supabase } from "../lib/supabaseClient";
 
 const NewGroupPage = () => {
   const { data: session } = useSession();
+  const [groupName, setGroupName] = useState("");
   const [emails, setEmails] = useState([""]);
 
   const addEmailField = () => {
     setEmails([...emails, ""]);
+  };
+
+  const handleEmailChange = (
+    index: number,
+    value: string
+  ) => {
+    const updatedEmails = [...emails];
+    updatedEmails[index] = value;
+    setEmails(updatedEmails);
+  };
+
+  const handleSubmit = async (e: {
+    preventDefault: () => void;
+  }) => {
+    e.preventDefault();
+    if (!session?.user?.email)
+      return alert("you must be signed it");
+
+    // inserting new groups
+    const { data: groupData, error: groupError } =
+      await supabase
+        .from("groups")
+        .insert([{ name: groupName }])
+        .select()
+        .single();
+    if (groupError) {
+      console.error("group error: ", groupError);
+      return;
+    }
+
+    // inserting emails into "group members"
+    const allEmails = [session.user.email, ...emails];
+    const { data, error } = await supabase
+      .from("group_members")
+      .insert(
+        allEmails
+          .map((email) => email.trim())
+          .filter((email) => email !== "")
+          .map((email) => ({
+            group_id: groupData.id,
+            email,
+          }))
+      );
+    if (error) {
+      console.error("error adding group members: ", error);
+    } else {
+      console.log("Group created successfully: ", data);
+    }
   };
 
   return (
@@ -17,11 +67,17 @@ const NewGroupPage = () => {
           <h1 className="text-5xl font-bold m-5">
             Invite People
           </h1>
-          <form className="flex flex-col gap-4">
+          <form
+            onSubmit={handleSubmit}
+            className="flex flex-col gap-4"
+          >
             <input
               type="text"
               placeholder="Group Name"
               className="input input-bordered input-accent w-full max-w-xs"
+              value={groupName}
+              onChange={(e) => setGroupName(e.target.value)}
+              required
             />
 
             {emails.map((email, index) => (
@@ -30,6 +86,10 @@ const NewGroupPage = () => {
                 type="email"
                 placeholder="Invitee Email"
                 className="input input-bordered input-accent w-full max-w-xs"
+                value={email}
+                onChange={(e) =>
+                  handleEmailChange(index, e.target.value)
+                }
               />
             ))}
 
